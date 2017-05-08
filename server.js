2dd64207adf2c4f77558fa11a4fdd12302728d52
sankyyu <syu10@stevens.edu>
2017-05-08 18:19:35 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,9 +1,51 @@
 const express=require('express');
+const hbs=require('hbs');
+const fs=require('fs');
 
 var app=express();
 
-app.get('/',(req,res)=>{
-  res.send('Hello Express');
-})
+hbs.registerPartials(__dirname+'/views/partials')
+app.set('view engine','hbs');
 
-app.listen(4000);
+app.use((req,res,next)=>{
+  var now=new Date().toString();
+  var log=now+':'+req.method+req.url;
+  console.log(log);
+
+  fs.appendFile('server.log',log+'\n');
+  next();
+});
+
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
